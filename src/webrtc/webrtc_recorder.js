18d5f59746f0137a005bdf50f40e0141bde0009b
Rashad Aliyev <aliyev.resad@gmail.com>
2020-01-26 12:42:41 +0400

    Added restrict mirroring for screen recorder

--- src/webrtc/webrtc_recorder.js
+++ src/webrtc/webrtc_recorder.js
@@ -24,6 +24,8 @@ Scoped.define("module:WebRTC.RecorderWrapper", [
                 this._hasVideo = false;
                 this._screen = options.screen;
                 this._flip = !!options.flip;
+                if (this._screen && !options.flipcreenrecorder)
+                    this._flip = false;
                 this._videoTrackSettings = {
                     slippedFromOrigin: {
                         height: 1.00,
