cc092b48d8d06664b3481aecdd712eb6775fbd78
Natasha Calleia <natasha@calleia.com.br>
2022-04-15 13:12:05 -0300

    Allow manual quality selection for HLS

--- src/video_player/video_player.js
+++ src/video_player/video_player.js
@@ -341,9 +341,23 @@ Scoped.define("module:Player.Html5VideoPlayerWrapper", [
                                 this._hls = new Hls();
                                 this._hls.on(Hls.Events.MEDIA_ATTACHED, function() {
                                     this._hls.loadSource(source.src);
-                                    this._hls.on(Hls.Events.MANIFEST_PARSED, function() {
+                                    this._hls.on(Hls.Events.MANIFEST_PARSED, function(_, data) {
+                                        this._qualityOptions = data.levels.map(function(level, index) {
+                                            return {
+                                                id: index,
+                                                label: level.width + "x" + level.height + " (" + Math.round(level.bitrate / 1024) + " kbps)"
+                                            };
+                                        });
+                                        this._currentQuality = this._qualityOptions[this._hls.startLevel];
+                                        this._hls.on(Hls.Events.LEVEL_SWITCHED, function(_, data) {
+                                            this._currentQuality = this._qualityOptions[data.level];
+                                            this.trigger("qualityswitched", this._qualityOptions[data.level]);
+                                        }.bind(this));
+                                        this.on("setsourcequality", function(quality) {
+                                            this._hls.currentLevel = quality;
+                                        });
                                         promise.asyncSuccess(true);
-                                    });
+                                    }.bind(this));
                                 }.bind(this));
                                 this._hls.on(Hls.Events.ERROR, function(e, data) {
                                     var error;
